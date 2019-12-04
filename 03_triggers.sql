34503fc016af1e8e345a79ae2dbc1ee1900d82c9
Jiří Hejduk <jiri@hejduk.in>
2019-12-04 19:58:17 +0100

    new trigger for INSERT FILES

--- 03_triggers.sql
+++ 03_triggers.sql
@@ -13,65 +13,72 @@ CREATE OR REPLACE TRIGGER pmsgs_insert_trg
         :new.PMSG_ID := pmsgs_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER students_new_trg
+CREATE OR REPLACE TRIGGER students_insert_trg
     BEFORE INSERT ON STUDENTS
     FOR EACH ROW WHEN (new.STUDENT_ID IS NULL)
     BEGIN
         :new.STUDENT_ID := students_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER courses_new_trg
+CREATE OR REPLACE TRIGGER courses_insert_trg
     BEFORE INSERT ON COURSES
     FOR EACH ROW WHEN (new.COURSE_ID IS NULL)
     BEGIN
         :new.COURSE_ID := courses_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER teachers_new_trg
+CREATE OR REPLACE TRIGGER teachers_insert_trg
     BEFORE INSERT ON TEACHERS
     FOR EACH ROW WHEN (new.TEACHER_ID IS NULL)
     BEGIN
         :new.TEACHER_ID := teachers_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER grades_new_trg
+CREATE OR REPLACE TRIGGER grades_insert_trg
     BEFORE INSERT ON GRADES
     FOR EACH ROW WHEN (new.GRADE_ID IS NULL)
     BEGIN
         :new.GRADE_ID := grades_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER groups_new_trg
+CREATE OR REPLACE TRIGGER groups_insert_trg
     BEFORE INSERT ON GROUPS
     FOR EACH ROW WHEN (new.GROUP_ID IS NULL)
     BEGIN
         :new.GROUP_ID := groups_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER classrooms_new_trg
+CREATE OR REPLACE TRIGGER classrooms_insert_trg
     BEFORE INSERT ON CLASSROOMS
     FOR EACH ROW WHEN (new.CLASSROOM_ID IS NULL)
     BEGIN
         :new.CLASSROOM_ID := classrooms_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER gmsg_new_trg
+CREATE OR REPLACE TRIGGER gmsg_insert_trg
     BEFORE INSERT ON GROUP_MESSAGES
     FOR EACH ROW WHEN (new.GMSG_ID IS NULL)
     BEGIN
         :new.GMSG_ID := gmsgs_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER timetables_new_trg
+CREATE OR REPLACE TRIGGER timetables_insert_trg
     BEFORE INSERT ON TIMETABLES
     FOR EACH ROW WHEN (new.TIMETABLE_ID IS NULL)
     BEGIN
         :new.TIMETABLE_ID := timetables_id_seq.nextval;
     END;
 
-CREATE OR REPLACE TRIGGER comments_new_trg
+CREATE OR REPLACE TRIGGER comments_insert_trg
     BEFORE INSERT ON COMMENTS
     FOR EACH ROW WHEN (new.COMMENT_ID IS NULL)
     BEGIN
         :new.COMMENT_ID := comments_id_seq.nextval;
+    END;
+
+CREATE OR REPLACE TRIGGER files_insert_trg
+    BEFORE INSERT ON FILES
+    FOR EACH ROW WHEN (new.FILE_ID IS NULL)
+    BEGIN
+        :new.FILE_ID := FILES_ID_SEQ.nextval;
     END;
